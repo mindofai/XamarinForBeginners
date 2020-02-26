a5db9a4a24545b0c4350780f9456c4c4787f553b
Mark Erwin C. Villarina <me.villarina@live.com>
2020-02-26 22:20:27 +0800

    After

--- MyAnimeList/MyAnimeList/ViewModels/AnimeWebsitePageViewModel.cs
+++ MyAnimeList/MyAnimeList/ViewModels/AnimeWebsitePageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Commands;
+﻿using MyAnimeList.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
@@ -11,6 +12,20 @@ namespace MyAnimeList.ViewModels
     {
         public AnimeWebsitePageViewModel(INavigationService navigationService) : base(navigationService)
         {
+        }
+
+        private AnimeDetailsModel _selectedAnime;
+        public AnimeDetailsModel SelectedAnime
+        {
+            get => _selectedAnime;
+            set => SetProperty(ref _selectedAnime, value);
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            SelectedAnime = parameters.GetValue<AnimeDetailsModel>("SelectedAnime");
         }
     }
 }
--- MyAnimeList/MyAnimeList/ViewModels/SearchAnimePageViewModel.cs
+++ MyAnimeList/MyAnimeList/ViewModels/SearchAnimePageViewModel.cs
@@ -5,6 +5,7 @@ using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyAnimeList.ViewModels
 {
@@ -12,19 +13,25 @@ namespace MyAnimeList.ViewModels
     {
         public SearchAnimePageViewModel(INavigationService navigationService) : base(navigationService)
         {
-            
+            ItemTappedCommand = new DelegateCommand<AnimeDetailsModel>(async (item) => await ExecuteItemTappedCommand(item));
         }
 
-        private void SetData()
+
+        private async Task AcquireData()
         {
+            //Add Internet Checking 
+            //Add API
+
             AnimeDetails = new List<AnimeDetailsModel>()
             {
-                new AnimeDetailsModel() { Id = 1, Title = "One Piece", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSource = "https://m.media-amazon.com/images/M/MV5BODNmZWRlN2ItMmRmYy00MWM1LTllMGQtMWY4NzgwNTU2MmY5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", Rating = 1.0, Website = "https://www.viz.com/one-piece" },
-                new AnimeDetailsModel() { Id = 2, Title = "One Piece GOLD", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSource = "https://m.media-amazon.com/images/M/MV5BODNmZWRlN2ItMmRmYy00MWM1LTllMGQtMWY4NzgwNTU2MmY5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", Rating = 2.0, Website = "https://www.viz.com/one-piece" },
-                new AnimeDetailsModel() { Id = 3, Title = "One Piece SILVER", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSource = "https://m.media-amazon.com/images/M/MV5BODNmZWRlN2ItMmRmYy00MWM1LTllMGQtMWY4NzgwNTU2MmY5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", Rating = 3.0, Website = "https://www.viz.com/one-piece" },
-                new AnimeDetailsModel() { Id = 3, Title = "One Piece PLATINUM", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSource = "https://m.media-amazon.com/images/M/MV5BODNmZWRlN2ItMmRmYy00MWM1LTllMGQtMWY4NzgwNTU2MmY5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", Rating = 4.0, Website = "https://www.viz.com/one-piece" },
-                 new AnimeDetailsModel() { Id = 3, Title = "One Piece EMERALD", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSource = "https://m.media-amazon.com/images/M/MV5BODNmZWRlN2ItMmRmYy00MWM1LTllMGQtMWY4NzgwNTU2MmY5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", Rating = 5.0, Website = "https://www.viz.com/one-piece" }
+                new AnimeDetailsModel() { Id = 1, Title = "One Piece", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSrc = "https://seeklogo.com/images/X/xamarin-logo-348B1EB629-seeklogo.com.png", Rating = 1.0, Website = "https://www.viz.com/one-piece" },
+                new AnimeDetailsModel() { Id = 2, Title = "One Piece GOLD", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSrc = "https://seeklogo.com/images/X/xamarin-logo-348B1EB629-seeklogo.com.png", Rating = 2.0, Website = "https://www.viz.com/one-piece" },
+                new AnimeDetailsModel() { Id = 3, Title = "One Piece SILVER", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSrc = "https://seeklogo.com/images/X/xamarin-logo-348B1EB629-seeklogo.com.png", Rating = 3.0, Website = "https://www.viz.com/one-piece" },
+                new AnimeDetailsModel() { Id = 3, Title = "One Piece PLATINUM", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSrc = "https://seeklogo.com/images/X/xamarin-logo-348B1EB629-seeklogo.com.png", Rating = 4.0, Website = "https://www.viz.com/one-piece" },
+                 new AnimeDetailsModel() { Id = 3, Title = "One Piece EMERALD", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSrc = "https://seeklogo.com/images/X/xamarin-logo-348B1EB629-seeklogo.com.png", Rating = 5.0, Website = "https://www.viz.com/one-piece" }
             };
+
+            await Task.FromResult(0);
         }
 
         private List<AnimeDetailsModel> _animeDetails;
@@ -34,11 +41,24 @@ namespace MyAnimeList.ViewModels
             set => SetProperty(ref _animeDetails, value);
         }
 
-        public override void OnNavigatedTo(INavigationParameters parameters)
+        public DelegateCommand<AnimeDetailsModel> ItemTappedCommand { get; private set; }
+
+        private async Task ExecuteItemTappedCommand(AnimeDetailsModel item)
+        {
+            var navParameters = new NavigationParameters();
+            navParameters.Add("SelectedAnime", item);
+
+            await NavigationService.NavigateAsync("AnimeDetailsPage", navParameters);
+        }
+
+        public async override void OnNavigatedTo(INavigationParameters parameters)
         {
             base.OnNavigatedTo(parameters);
 
-            SetData();
+            if (AnimeDetails == null)
+            {
+                await AcquireData();
+            }
         }
     }
 }
