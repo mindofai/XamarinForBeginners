fff7ea652bca0db5c94795f5dd0b8bd99b2087f6
Mark Erwin C. Villarina <me.villarina@live.com>
2020-02-26 21:04:25 +0800

    Push Initial

---
+++ MyAnimeList/MyAnimeList/Models/AnimeDetailsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAnimeList.Models
+{
+    public class AnimeDetailsModel
+    {
+        public int Id { get; set; }
+        public string Website { get; set; }
+        public string Title { get; set; }
+        public string ImageSource { get; set; }
+        public string Synopsis { get; set; }
+        public double Rating { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
---
+++ MyAnimeList/MyAnimeList/ViewModels/SearchAnimePageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using MyAnimeList.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyAnimeList.ViewModels
+{
+    public class SearchAnimePageViewModel : ViewModelBase
+    {
+        public SearchAnimePageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            
+        }
+
+        private void SetData()
+        {
+            AnimeDetails = new List<AnimeDetailsModel>()
+            {
+                new AnimeDetailsModel() { Id = 1, Title = "One Piece", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSource = "https://m.media-amazon.com/images/M/MV5BODNmZWRlN2ItMmRmYy00MWM1LTllMGQtMWY4NzgwNTU2MmY5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", Rating = 1.0, Website = "https://www.viz.com/one-piece" },
+                new AnimeDetailsModel() { Id = 2, Title = "One Piece GOLD", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSource = "https://m.media-amazon.com/images/M/MV5BODNmZWRlN2ItMmRmYy00MWM1LTllMGQtMWY4NzgwNTU2MmY5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", Rating = 2.0, Website = "https://www.viz.com/one-piece" },
+                new AnimeDetailsModel() { Id = 3, Title = "One Piece SILVER", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSource = "https://m.media-amazon.com/images/M/MV5BODNmZWRlN2ItMmRmYy00MWM1LTllMGQtMWY4NzgwNTU2MmY5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", Rating = 3.0, Website = "https://www.viz.com/one-piece" },
+                new AnimeDetailsModel() { Id = 3, Title = "One Piece PLATINUM", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSource = "https://m.media-amazon.com/images/M/MV5BODNmZWRlN2ItMmRmYy00MWM1LTllMGQtMWY4NzgwNTU2MmY5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", Rating = 4.0, Website = "https://www.viz.com/one-piece" },
+                 new AnimeDetailsModel() { Id = 3, Title = "One Piece EMERALD", Synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", StartDate = "01/01/1990", EndDate = "12/12/2019", ImageSource = "https://m.media-amazon.com/images/M/MV5BODNmZWRlN2ItMmRmYy00MWM1LTllMGQtMWY4NzgwNTU2MmY5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", Rating = 5.0, Website = "https://www.viz.com/one-piece" }
+            };
+        }
+
+        private List<AnimeDetailsModel> _animeDetails;
+        public List<AnimeDetailsModel> AnimeDetails
+        {
+            get => _animeDetails;
+            set => SetProperty(ref _animeDetails, value);
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            SetData();
+        }
+    }
+}
